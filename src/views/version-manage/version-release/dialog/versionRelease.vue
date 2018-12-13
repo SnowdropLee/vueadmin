2d8325b05771c36b0ef1914ed4a5bef369ce284a
lilifu <lilifu@agree.com.cn>
2018-12-13 20:26:37 +0800

    版本发布页面弹窗 修改'发布规则' 为 '信息确认'

--- src/views/version-manage/version-release/dialog/versionRelease.vue
+++ src/views/version-manage/version-release/dialog/versionRelease.vue
@@ -1,15 +1,30 @@
 <template>
   <div>
-    <el-dialog title="版本发布" :visible.sync="isShow" @close="closeCallback" center :lock-scroll=false :close-on-click-modal="false" width="800px">
+    <el-dialog
+      title="版本发布"
+      :visible.sync="isShow"
+      @close="closeCallback"
+      center
+      :lock-scroll="false"
+      :close-on-click-modal="false"
+      width="800px"
+    >
       <div v-loading="loading" element-loading-text="拼命加载中">
         <el-steps :active="active" align-center style="margin-bottom:10px" finish-status="success">
           <el-step title="发布类型"></el-step>
-          <el-step title="发布规则"></el-step>
+          <el-step title="信息确认"></el-step>
         </el-steps>
 
         <div v-if="active === 0">
           <el-card style="margin-bottom:10px;overflow:visible;">
-            <el-form :model="form" :rules="rules" ref="form" size="mini" label-width="90px" label-position="left">
+            <el-form
+              :model="form"
+              :rules="rules"
+              ref="form"
+              size="mini"
+              label-width="90px"
+              label-position="left"
+            >
               <el-row :gutter="10">
                 <!-- <el-col :md="12" :lg="12">
                 <el-form-item label="设备分类">
@@ -18,12 +33,16 @@
                     <el-option label="区域二" value="beijing"></el-option>
                   </el-select>
                 </el-form-item>
-              </el-col> -->
+                </el-col>-->
                 <el-col :span="12">
                   <el-form-item label="版本编号 : " prop="versionCode">
                     <el-select v-model="form.versionCode" placeholder="请选择版本编号" clearable>
-                      <el-option v-for="item in verBaseInfo" :key="item.adID" :label="item.versionCode" :value="item.versionCode">
-                      </el-option>
+                      <el-option
+                        v-for="item in verBaseInfo"
+                        :key="item.adID"
+                        :label="item.versionCode"
+                        :value="item.versionCode"
+                      ></el-option>
                     </el-select>
                   </el-form-item>
                 </el-col>
@@ -40,73 +59,144 @@
 
                 <el-col :span="12" v-if="radio == '0'">
                   <el-form-item label="分组 : " prop="area">
-                    <el-select v-model="form.area" placeholder="请选择分组">
-                      <el-option v-for="item in areaList" :key="item.branchAreaId" :label="item.branchAreaName" :value="item.branchAreaId"></el-option>
+                    <el-select v-model="form.area" placeholder="请选择分组" clearable @change="groupChange">
+                      <el-option
+                        v-for="item in areaList"
+                        :key="item.branchAreaId"
+                        :label="item.branchAreaName"
+                        :value="item.branchAreaId"
+                      ></el-option>
                     </el-select>
                   </el-form-item>
                 </el-col>
                 <el-col :span="12" v-if="radio == '1'">
                   <el-form-item label="机构 : " prop="selBrno">
-                    <select-tree :tree-data="brnoList" v-model="form.selBrno" :node-key="'branchId'" :props="{id:'branchId',label:'branchName',children:'childBranch'}"></select-tree>
+                    <select-tree
+                      :tree-data="brnoList"
+                      v-model="form.selBrno"
+                      :node-key="'branchId'"
+                      :props="{id:'branchId',label:'branchName',children:'childBranch'}"
+                      @select-check="selectChange"
+                    ></select-tree>
                   </el-form-item>
                 </el-col>
               </el-row>
 
               <el-row v-if="radio=='2'">
                 <el-col>
-                  <el-table :data="devTableData" header-row-class-name="tableHeaderClass" ref="multipleTable" @selection-change="handleSelectionChange" border>
+                  <el-table
+                    :data="devTableData"
+                    header-row-class-name="tableHeaderClass"
+                    ref="multipleTable"
+                    @selection-change="handleSelectionChange"
+                    border
+                  >
                     <el-table-column type="selection" width="40"></el-table-column>
                     <el-table-column fixed prop="devnum" label="设备编号" width="120"></el-table-column>
-                    <el-table-column prop="devtype" label="设备类型" width="120"> </el-table-column>
-                    <el-table-column prop="branchname" label="及其所在网点" width="150"> </el-table-column>
-                    <el-table-column prop="devip" label="设备IP" min-width="120"> </el-table-column>
+                    <el-table-column prop="devtype" label="设备类型" width="120"></el-table-column>
+                    <el-table-column prop="branchname" label="及其所在网点" width="150"></el-table-column>
+                    <el-table-column prop="devip" label="设备IP" min-width="120"></el-table-column>
                   </el-table>
-                  <el-pagination style="text-align: right;margin-top:20px;" @current-change="handleCurrentChange" @size-change="handleSizeChange" :current-page.sync="currentPage" :page-sizes="[10, 20, 50,100]" :page-size="pageSize" background layout="total, prev, pager, next, jumper" :total="tableTotal">
-                  </el-pagination>
+                  <el-pagination
+                    style="text-align: right;margin-top:20px;"
+                    @current-change="handleCurrentChange"
+                    @size-change="handleSizeChange"
+                    :current-page.sync="currentPage"
+                    :page-sizes="[10, 20, 50,100]"
+                    :page-size="pageSize"
+                    background
+                    layout="total, prev, pager, next, jumper"
+                    :total="tableTotal"
+                  ></el-pagination>
                 </el-col>
               </el-row>
-
             </el-form>
           </el-card>
           <div slot="footer" class="dialog-footer" style="text-align:center;">
-            <el-button type="primary" size="mini" style="margin-top: 10px;" @click="next('form')">下一步</el-button>
+            <el-button
+              type="primary"
+              size="mini"
+              style="margin-top: 10px;"
+              @click="next('form')"
+            >下一步</el-button>
           </div>
         </div>
 
         <div v-if="active === 1">
           <el-card style="margin-bottom:10px;overflow:visible;">
-            <el-form :model="form" :rules="rules" ref="form" size="mini" label-width="110px" label-position="left">
-              <el-row :gutter="20">
-                <el-col :span="10">
+            <el-form
+              :model="form"
+              :rules="rules"
+              ref="form"
+              size="mini"
+              label-width="110px"
+              label-position="top"
+            >
+              <!-- <el-row :gutter="20"> -->
+              <!-- 立即生效 -->
+              <!-- <el-col :span="10">
                   <el-form-item label="立即生效 :" prop="isEffective">
                     <el-radio-group v-model="form.isEffective" @change="radioChange">
                       <el-radio :label="1">是</el-radio>
                       <el-radio :label="2">否</el-radio>
                     </el-radio-group>
                   </el-form-item>
-                </el-col>
-                <el-col :span="12" :offset="2">
+              </el-col>-->
+              <!-- 预计发布时间 -->
+              <!-- <el-col :span="12" :offset="2">
                   <el-form-item label="预计发布时间:" prop="verUpdatePrepareTime">
                     <el-date-picker type="datetime" :readonly="isdatenow" placeholder="选择日期时间" value-format="yyyyMMddHHmmss" v-model="form.verUpdatePrepareTime" :disabled="form.isEffective==1">
                     </el-date-picker>
                   </el-form-item>
-                </el-col>
-                <el-col :span="12">
+              </el-col>-->
+              <!-- 升级间隔时间 -->
+              <!-- <el-col :span="12">
                   <el-form-item label="升级间隔时间:" prop="verUpdateInterval">
                     <el-input v-model="form.verUpdateInterval" placeholder="请输入升级间隔时间（秒）"></el-input>
                   </el-form-item>
-                </el-col>
-                <el-col :md="12" :lg="12">
+              </el-col>-->
+              <!-- 单次升级数量 -->
+              <!-- <el-col :md="12" :lg="12">
                   <el-form-item label="单次升级数量:" prop="verUpdateDevNum">
                     <el-input v-model="form.verUpdateDevNum" placeholder="请输入单次升级数量"></el-input>
                   </el-form-item>
-                </el-col>
-              </el-row>
+              </el-col>-->
+              <!-- </el-row> -->
+              <!-- 版本编号 -->
+              <el-form-item label="版本编号 :">
+                <el-input v-model="form.versionCode" disabled
+                ></el-input>
+              </el-form-item>
+
+              <!-- 发布方式 -->
+              <el-form-item label="机构 :" v-if="treeselectdata && radio==1">
+                <el-input  v-model="treeselectdata" disabled></el-input>
+              </el-form-item>
+              <el-form-item label="分组 :" v-if="areaName && radio==0">
+                <el-input  v-model="areaName" disabled></el-input>
+              </el-form-item>
+              <el-form-item label="设备 :" v-if="devList && radio==2">
+                <el-table
+                    :data="devList"
+                    min-height="200"
+                    border
+                  >
+                    <el-table-column fixed prop="devnum" label="设备编号" width="120"></el-table-column>
+                    <el-table-column prop="devtype" label="设备类型" width="120"></el-table-column>
+                    <el-table-column prop="branchname" label="及其所在网点" width="150"></el-table-column>
+                    <el-table-column prop="devip" label="设备IP" min-width="120"></el-table-column>
+                  </el-table>
+              </el-form-item>
             </el-form>
           </el-card>
           <div slot="footer" class="dialog-footer" style="text-align:center;">
-            <el-button @click='back' size="mini" type='primary' style="margin-top: 10px;">上一步</el-button>
-            <el-button @click="sure('form')" size="mini" type='primary' style="margin-top: 10px;">确 定</el-button>
+            <el-button @click="back" size="mini" type="primary" style="margin-top: 10px;">上一步</el-button>
+            <el-button
+              @click="sure('form')"
+              size="mini"
+              type="primary"
+              style="margin-top: 10px;"
+            >确 定</el-button>
           </div>
         </div>
       </div>
@@ -158,19 +248,23 @@ export default {
       isdatenow: false,
       pageSize: getGlobalParams.get("PageSize"),
       tableTotal: 0,
-      verBaseInfo: [], //
-      brnoList: [], // 机构下拉框数据
-      areaList: [],
+      verBaseInfo: [], //版本编号数据
+      brnoList: [], // 按机构选择-----机构数据树
+      areaList: [], // 按分组选择-----分组数据
       verUpdateDevList: [],
       form: {
-        selBrno: [],
-        versionCode: "",
-        area: "",
-        isEffective: 2,
-        verUpdatePrepareTime: "",
-        verUpdateInterval: "",
-        verUpdateDevNum: ""
+        selBrno: [], //机构选中的内容
+        versionCode: "", //版本编号
+        area: "", //分组选中的内容 ---->branchAreaId
+        isEffective: 2, // 立即生效
+        verUpdatePrepareTime: "", //预计发布时间
+        verUpdateInterval: "", //升级间隔时间
+        verUpdateDevNum: "" //单次升级数量
       },
+      treeselectdata: "", //按机构选择结果
+      areaName: "", //按组选择结果
+      devList:undefined, //按设备选择结果
+
       // 定义校验规则
       rules: {
         versionCode: [
@@ -233,6 +327,7 @@ export default {
       ];
       request(resBody)
         .then(response => {
+          // console.log(response);
           this.brnoList = response.RSP_BODY.branchSpinner;
           this.verBaseInfo = response.RSP_BODY.verBaseInfoSpinner;
           this.areaList = response.RSP_BODY.branchAreaList;
@@ -290,6 +385,7 @@ export default {
       resBody.QueryPageNo = this.currentPage;
       request(resBody)
         .then(response => {
+          // console.log(response)
           if (response.SYS_HEAD.ReturnCode === "000000") {
             this.devTableData = response.RSP_BODY.verUpdateDevInfoList;
             this.tableTotal = Number(
@@ -315,6 +411,7 @@ export default {
       this.queryInfoList();
     },
     handleSelectionChange(val) {
+      this.devList = val
       for (var i = 0; i < val.length; i++) {
         this.verUpdateDevList[i] = val[i].devid;
       }
@@ -364,13 +461,16 @@ export default {
     back() {
       this.verUpdateDevList = [];
       this.$refs["form"].clearValidate();
+      this.treeselectdata = ''
+      this.areaName = ""
+      this.devList = null
       if (this.active >= 0) this.active--;
     },
     sure(formName) {
       this.$refs[formName].validate(valid => {
         if (valid) {
           this.loading = true;
-          if (this.radio === 2) {
+          if (this.radio === 2) { 
             let resBody = new releasebydev();
             resBody.data.versionCode = this.form.versionCode;
             resBody.data.isEffective = this.form.isEffective + "";
@@ -453,6 +553,7 @@ export default {
             resBody.data.verUpdatePattern = this.radio + "";
             resBody.data.branchNo = this.$store.getters.branchNo;
             resBody.data.zoneNo = this.form.area;
+            console.log(resBody)
             request(resBody)
               .then(response => {
                 if (response.SYS_HEAD.ReturnCode === "000000") {
@@ -488,6 +589,29 @@ export default {
           return false;
         }
       });
+    },
+    //selecttree 方法
+    selectChange(checkedNode) {
+      var str = ''
+      if(checkedNode.length===1){
+        checkedNode.forEach(item=>{
+          str+=`${item.branchName}`
+        })
+        this.treeselectdata = str
+      }else{
+        checkedNode.forEach(item=>{
+          str+=`${item.branchName},`
+        })
+        this.treeselectdata = str
+        var arr = this.treeselectdata.split(',')
+        arr.length = arr.length-1
+        this.treeselectdata = arr.join(',')
+      }
+    },
+    // 按组选择方法
+    groupChange(val){
+      this.areaName = event.target.innerText
+      // console.dir(event.target.innerText)
     }
   }
 };
